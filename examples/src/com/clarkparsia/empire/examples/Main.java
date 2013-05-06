088ab0016018a146c696030fc8ef9321eba1a1bd
mhgrove <mike@clarkparsia.com>
2013-05-06 12:13:06 -0400

    fixing error in example that was accidentally committed

--- examples/src/com/clarkparsia/empire/examples/Main.java
+++ examples/src/com/clarkparsia/empire/examples/Main.java
@@ -47,12 +47,7 @@ public class Main {
 											.createEntityManager();
 
 		// this retrieves a particular book from the database
-		IBook aBook = aManager.find(IBook.class, URI.create("urn:x-domain:oreilly.com:product:9780596514129.IP"));
-        aBook = aManager.find(IBook.class, URI.create("urn:x-domain:oreilly.com:product:9780596514129.IP"));
-        aBook = aManager.find(IBook.class, URI.create("urn:x-domain:oreilly.com:product:9780596514129.IP"));
-        aBook = aManager.find(IBook.class, URI.create("urn:x-domain:oreilly.com:product:9780596514129.IP"));
-        aBook = aManager.find(IBook.class, URI.create("urn:x-domain:oreilly.com:product:9780596514129.IP"));
-        aBook = aManager.find(IBook.class, URI.create("urn:x-domain:oreilly.com:product:9780596514129.IP"));
+		Book aBook = aManager.find(Book.class, URI.create("urn:x-domain:oreilly.com:product:9780596514129.IP"));
 
 		// prints: Switching to the Mac: The Missing Manual, Leopard Edition
 		System.err.println(aBook.getTitle());

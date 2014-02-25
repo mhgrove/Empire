3162043033a6cfa21d1ec44d47a4bf91a2422b56
Alex Legler <alex@a3li.li>
2014-02-25 18:54:23 +0100

    Inherit @Namespaces
    DRY on namespace definitions

--- core/main/src/com/clarkparsia/empire/annotation/Namespaces.java
+++ core/main/src/com/clarkparsia/empire/annotation/Namespaces.java
@@ -16,6 +16,7 @@
 package com.clarkparsia.empire.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -41,6 +42,7 @@ import java.lang.annotation.Target;
  * @author Michael Grove
  * @since 0.1
  */
+@Inherited
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Namespaces {
